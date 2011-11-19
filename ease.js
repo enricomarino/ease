a1d953f0b8ea4251dac14573cacffc3ccb1b9f8b
Enrico Marino <onirame@gmail.com>
2011-11-19 19:01:24 +0100

    improved

--- ease.js
+++ ease.js
@@ -4,7 +4,7 @@
 // Copyright 2011 Enrico Marino
 // MIT license
 
-// The equations defined here are open source under BSD License.
+// inspired by robert penner easing equations
 // http://www.robertpenner.com/easing_terms_of_use.html 
 // Copyright 2003 Robert Penner
 
@@ -26,110 +26,138 @@
         PI_mul_2 = PI * 2;
 
     function blink (pos, blinks) {
+
         return round(pos * (blinks || 5)) % 2;
     }
 
     function bounce (t) {
-        if (t < (1 / 2.75)) {
+
+        if (t < 0.3636) { // t < 1 / 2.75
             return 7.5625 * t * t;
         }
-        if (t < (2 / 2.75)) {
-            return 7.5625 * (t -= (1.5 / 2.75)) * t + 0.75;
+        if (t < 0.7273) { // t < 2 / 2.75
+            t -= 0.5455; // t = t - 1.5 / 2.75
+            return 7.5625 * t * t + 0.75;
         }
-        if (t < (2.5 / 2.75)) {
-            return 7.5625 * (t -= (2.25 / 2.75)) * t + 0.9375;
+        if (t < 0.9091) { // t < 2.5 / 2.75
+            t -= 0.8182; // t = t - 2.25 / 2.75
+            return 7.5625 * t * t + 0.9375;
         }
-        return 7.5625 * (t -= (2.625 / 2.75)) * t + 0.984375;
+        t -= 0.9545; // t = t - 2.625 / 2.75
+        return 7.5625 * t * t + 0.9844;
     }
 
-    function bounce_past (pos) {
-        if (pos < (1 / 2.75)) {
-            return (7.5625 * pos * pos);
-        } else if (pos < (2 / 2.75)) {
-            return 2 - (7.5625 * (pos -= (1.5 / 2.75)) * pos + .75);
-        } else if (pos < (2.5 / 2.75)) {
-            return 2 - (7.5625 * (pos -= (2.25 / 2.75)) * pos + .9375);
-        } else {
-            return 2 - (7.5625 * (pos -= (2.625 / 2.75)) * pos + .984375);
+    function bounce_past (t) {
+
+        if (t < 0.3636) { // t < 1 / 2.75
+            return 7.5625 * t * t;
         }
+        if (t < 0.7273) { // t < 2 / 2.75
+            t -= 0.5455; // t = t - 1.5 / 2.75
+            return 2 - (7.5625 * t * t + 0.75);
+        }
+        if (t < 0.9091) { // t < 2.5 / 2.75
+            t -= 0.8182; // t = t - 2.25 / 2.75
+            return 2 - (7.5625 * t * t + 0.9375);
+        }
+        t -= 0.9545; // t = t - 2.625 / 2.75
+        return 2 - (7.5625 * t * t + 0.9844);
     }
 
-    function elastic (pos) {
-        return -1 * pow(4, -8 * pos) * sin((pos * 6 - 1) * (2 * PI) / 2) + 1;
+    function elastic (t) {
+
+        return -1 * pow(4, -8 * t) * sin((t * 6 - 1) * PI) + 1;
     }
 
-    function flicker (pos) {
-        var pos = pos + (random() - 0.5)/5;
-        return sinusoidal(pos < 0 ? 0 : pos > 1 ? 1 : pos);
+    function flicker (t) {
+
+        t = t + (random() - 0.5) * 0.2;
+        return sinusoidal(t < 0 ? 0 : t > 1 ? 1 : t);
     }
 
-    function in_back (pos) {
+    function in_back (t) {
+
         var s = 1.70158;
-        return pos * pos * ((s + 1) * pos - s);
+        return t * t * ((s + 1) * t - s);
     }
 
     function in_strong (t) {
+
         return (t === 0) ? 0 : pow(2, 10 * (t - 1));
     }
 
-    function mirror (pos) {
-        if (pos < 0.5) {
-            return sinusoidal(pos * 2);
+    function mirror (t) {
+
+        if (t < 0.5) {
+            return sinusoidal(t * 2);
         }
-        return sinusoidal(1 - (pos - 0.5) * 2);
+        return sinusoidal(1 - (t - 0.5) * 2);
     }
 
     function out (t) {
+
         return sin(t * PI_div_2);
     }
 
     function out_back (pos) {
-        var s = 1.70158,
-            pos = pos - 1;
-        return pos * pos * ((s + 1) * pos + s) + 1;
+
+        var s = 1.70158;
+        t -= 1;
+        return t * t * ((s + 1) * t + s) + 1;
     }
 
     function out_bonce (pos) {
-        if ((pos) < (1/2.75)) {
-            return (7.5625 * pos * pos);
-        } else if (pos < (2/2.75)) {
-            return (7.5625 * (pos-=(1.5/2.75)) * pos + .75);
-        } else if (pos < (2.5/2.75)) {
-            return (7.5625 * (pos-=(2.25/2.75)) * pos + .9375);
-        } else {
-            return (7.5625 * (pos-=(2.625/2.75)) * pos + .984375);
+
+        if (t < 0.3636) { // t < 1 / 2.75
+            return 7.5625 * t * t;
+        } else if (t < 0.7273) { // t < 2 / 2.75
+            t -= 0.5455; // t = t - 1.5 / 2.75
+            return 7.5625 * t * t + 0.75;
+        } else if (t < 0.9091) { // t < 2.5 / 2.75
+            t -= 0.8182; // t = t - 2.25 / 2.75
+            return 7.5625 * t * t + 0.9375);
         }
+        t -= 0.9545; // t = t - 2.625 / 2.75
+        return 7.5625 * t * t + 0.9844;
     }
 
     function out_strong (t) {
+
         return (t === 1) ? 1 : 1 - pow(2, -10 * t);
     }
 
-    function pulse (pos, pulses) {
-        return (-cos((pos * ((pulses || 5) - 0.5) * 2) * PI)/2) + 0.5;
+    function pulse (t, pulses) {
+
+        pulse = pulse || 5;
+        return 0.5 - cos((t * (pulses - 0.5) * 2) * PI) * 0.5;
     }
 
-    function sinusoidal (pos) {
-        return (-cos(pos * PI)/2) + 0.5;
+    function sinusoidal (t) {
+
+        return 0.5 -cos(t * PI) * 0.5;
     }
 
-    function spring (pos) {
-        return 1 - (cos(pos * 4.5 * PI) * exp(-pos * 6));
+    function spring (t) {
+
+        return 1 - (cos(t * 4.5 * PI) * exp(-t * 6));
     }
 
-    function swing_from (pos) {
+    function swing_from (t) {
+
         var s = 1.70158;
-        return pos * pos * ((s + 1) * pos - s);
+        return t * t * ((s + 1) * t - s);
     }
 
-    function swing_to (pos) {
-        var s = 1.70158,
-            pos = pos - 1;
-        return pos * pos * ((s + 1) * pos + s) + 1;
+    function swing_to (t) {
+
+        var s = 1.70158;
+        t -= 1;
+        return t * t * ((s + 1) * t + s) + 1;
     }
 
-    function wobble (pos) {
-        return (-cos(pos * PI * (9 * pos))/2) + 0.5;
+    function wobble (t) {
+
+        return 0.5 -cos(t * PI * (9 * t)) * 0.5);
     }
 
     return {
